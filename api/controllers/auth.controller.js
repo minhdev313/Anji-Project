420bfa1b02403da595dfb9ab6b07524a688ae40c
minhdev313 <voducminh313@gmail.com>
2024-05-12 16:55:05 +0700

    handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,15 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { userName, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ userName, email, password: hashedPassword });
     try {
         await newUser.save()
-        res.status(201).json({ message: "User created successfully " });
+        res.status(201).json({ message: 'User created successfully'});
     } catch (error) {
-        res.status(500).json(error.message);
+        next(errorHandler(300, "Something not right"));
     }
-
-
 };
--- api/index.js
+++ api/index.js
@@ -13,7 +13,6 @@ mongoose.connect(process.env.MONGODB).then(() => {
 
 const app = express();
 
-
 app.use(express.json());
 
 app.listen(3000, () => {
@@ -22,3 +21,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) =>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
