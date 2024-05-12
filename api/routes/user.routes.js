851a135c9a039370d204755a91203bfbee15061a
minhdev313 <voducminh313@gmail.com>
2024-05-12 15:49:41 +0700

    test api route

---
+++ api/routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        message: 'API is working',
+    });
+});
+
+export default router
