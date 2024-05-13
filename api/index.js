ec28bc386ec7288634976e939f1c775be07433d4
minhdev313 <voducminh313@gmail.com>
2024-05-14 00:15:28 +0700

    Fix server

--- api/index.js
+++ api/index.js
@@ -17,24 +17,20 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname), 'client/dist'))
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
-
 app.use('/api/user', userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.use((err, req, res, next) =>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
@@ -42,4 +38,8 @@ app.use((err, req, res, next) =>{
     message,
     statusCode,
   });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
