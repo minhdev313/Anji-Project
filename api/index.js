d56a216f563b74c024c20cd2e064b1a9213bb399
Tammv <72090950+tammv@users.noreply.github.com>
2024-05-19 21:16:34 +0700

    123

--- api/index.js
+++ api/index.js
@@ -1,21 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.routes.js';
-import authRoutes from './routes/auth.route.js';
-import dishRoutes from './routes/dishes.route.js';
-import restaurantRoutes from './routes/restaurant.route.js';
-import categoryRoutes from './routes/category.route.js';
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import dishRoutes from "./routes/dishes.route.js";
+import restaurantRoutes from "./routes/restaurant.route.js";
+import categoryRoutes from "./routes/category.route.js";
 
 import cookieParser from "cookie-parser";
 import path from "path";
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB).then(() => {
-  console.log('Connected to MongoDB')
-}).catch((err) => {
-  console.log(err);
-})
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const __dirname = path.resolve();
 
@@ -31,16 +34,15 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use('/api/dishes', dishRoutes);
+app.use("/api/dishes", dishRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/restaurant", restaurantRoutes);
 
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     message,
