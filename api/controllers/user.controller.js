51af474b92d1a6b1af8e3c61a6828772eff25c73
Tammv <72090950+tammv@users.noreply.github.com>
2024-05-21 21:09:15 +0700

    update

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,10 +1,10 @@
-import User from '../models/user.model.js';
-import { errorHandler } from '../utils/error.js';
-import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
 
 export const test = (req, res) => {
   res.json({
-    message: 'API is working!',
+    message: "API is working!",
   });
 };
 
@@ -12,7 +12,7 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, 'You can update only your account!'));
+    return next(errorHandler(401, "You can update only your account!"));
   }
   try {
     if (req.body.password) {
@@ -26,6 +26,7 @@ export const updateUser = async (req, res, next) => {
           userName: req.body.userName,
           email: req.body.email,
           password: req.body.password,
+          level: req.body.level,
           profilePicture: req.body.profilePicture,
         },
       },
@@ -38,19 +39,16 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
 // delete user
-
 
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, 'You can delete only your account!'));
+    return next(errorHandler(401, "You can delete only your account!"));
   }
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.status(200).json('User has been deleted...');
+    res.status(200).json("User has been deleted...");
   } catch (error) {
     next(error);
   }
-
-}
+};
--- api/index.js
+++ api/index.js
@@ -24,10 +24,10 @@ const __dirname = path.resolve();
 
 const app = express();
 
- app.use(express.static(path.join(__dirname, "client", "dist")));
+//  app.use(express.static(path.join(__dirname, "client", "dist")));
 
- app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+//  app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 
 app.use(express.json());
 
