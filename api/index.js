735ee26a97710b15489d4c4418363f9b32c4a20e
Tammv <72090950+tammv@users.noreply.github.com>
2024-05-22 22:16:31 +0700

    add ;

--- api/index.js
+++ api/index.js
@@ -11,7 +11,8 @@ import cookieParser from "cookie-parser";
 import path from "path";
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB)
+mongoose
+  .connect(process.env.MONGODB)
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -22,31 +23,32 @@ mongoose.connect(process.env.MONGODB)
 const __dirname = path.resolve();
 const app = express();
 
- app.use(express.static(path.join(__dirname, "client", "dist")));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
- app.get("*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 
-app.use(express.json());
+  app.use(express.json());
 
-app.use(cookieParser());
+  app.use(cookieParser());
 
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/dishes", dishRoutes);
-app.use("/api/category", categoryRoutes);
-app.use("/api/restaurant", restaurantRoutes);
+  app.use("/api/user", userRoutes);
+  app.use("/api/auth", authRoutes);
+  app.use("/api/dishes", dishRoutes);
+  app.use("/api/category", categoryRoutes);
+  app.use("/api/restaurant", restaurantRoutes);
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({
-    success: false,
-    message,
-    statusCode,
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      statusCode,
+    });
   });
-});
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+  app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+  });
 });
