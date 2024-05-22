10d6a5640348a768db3c036dd350a8299cf4a94c
minhdev313 <103765171+minhdev313@users.noreply.github.com>
2024-05-22 22:27:18 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,13 +12,13 @@ import path from "path";
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGODB)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const __dirname = path.resolve();
 const app = express();
@@ -26,29 +26,29 @@ const app = express();
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+app.use(express.json());
 
-  app.use(express.json());
+app.use(cookieParser());
 
-  app.use(cookieParser());
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/dishes", dishRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/restaurant", restaurantRoutes);
 
-  app.use("/api/user", userRoutes);
-  app.use("/api/auth", authRoutes);
-  app.use("/api/dishes", dishRoutes);
-  app.use("/api/category", categoryRoutes);
-  app.use("/api/restaurant", restaurantRoutes);
-
-  app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
     return res.status(statusCode).json({
-      success: false,
-      message,
-      statusCode,
+        success: false,
+        message,
+        statusCode,
     });
-  });
-
-  app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-  });
 });
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
+
