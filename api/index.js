cf62e23cb2f61d7124dbb85451ce5a1a2fa189b6
minhdev313 <voducminh313@gmail.com>
2024-06-06 22:56:41 +0700

    Home.jxpx

--- api/index.js
+++ api/index.js
@@ -70,7 +70,7 @@ app.use((err, req, res, next) => {
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'Home.jsx'));
 });
 
 // Start server
--- client/vite.config.js
+++ client/vite.config.js
@@ -12,11 +12,6 @@ export default defineConfig({
         target: "http://localhost:3000",
         secure: false,
       },
-      // '/api': {
-      //   target: 'https://pay.payos.vn',
-      //   // changeOrigin: true,
-      //   secure: false
-      // }
     },
   },
   plugins: [react(), reactRefresh()],
