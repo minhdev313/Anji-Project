21a4527bd476d7fa0459ea196e2be22d864943d2
minhdev313 <voducminh313@gmail.com>
2024-06-07 19:57:42 +0700

    update link payment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -59,9 +59,9 @@ export default function Home() {
             <div className="text-center sm:flex sm:items-center sm:justify-center sm:text-left">
               <h2 className="text-4xl font-bold text-white">Get full access to Anji</h2>
               {currentUser ? (
-                /* <form action="https://anji-5vgz.onrender.com/payment"> */
+                <form action="https://anji-5vgz.onrender.com/payment">
                 /* Dung cho localhost */
-                < form action="http://localhost:3000/payment">
+                {/* // < form action="http://localhost:3000/payment"> */}
                   <button
                     type="submit" id="create-payment-link-btn"
                     className="inline-flex items-center justify-center flex-shrink-0 px-4 py-4 mt-8 text-base font-semibold text-gray-900 transition-all duration-200 bg-yellow-300 rounded-md sm:mt-0 sm:ml-8 lg:ml-16 hover:bg-yellow-400 focus:bg-yellow-400"
