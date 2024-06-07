36f2f1778e9d08655e080d898694514f919738ea
minhdev313 <voducminh313@gmail.com>
2024-06-08 02:24:19 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -67,10 +67,10 @@ app.use((err, req, res, next) => {
 });
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client'));
+  res.sendFile(path.join(__dirname, 'client', 'dist'));
 });
 
 // Start server
